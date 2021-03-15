6dcf515ad796c774bf3245c61ee94c2b6684da06
AaronNovakDev <aaronnovakdevelopements@gmail.com>
2021-03-15 21:34:33 +1000

    before bed commit, need to fix up Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'items/page'
   resources :items
   devise_for :users
+
   root 'home#page'
+
+
+ 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
