8b94c12bb79649d2bf0701f149476330e9b4dc90
AaronNovakDev <aaronnovakdevelopements@gmail.com>
2021-03-16 19:18:08 +1000

    trying to deploy

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module OutVeggin
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.serve_static_assets = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
